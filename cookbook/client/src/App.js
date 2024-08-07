b4fde399921005478736d646deebc258be267f74
Petr Jagos <jagos.p@email.cz>
2024-08-07 08:36:17 +0200

    1st Block - Individual work - added React to project, edit of App.js file

---
+++ cookbook/client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Toto je <span style={{ color: 'brown' }}>KUCHAŘKA</span></h1>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque sapien. Sed ac dolor sit amet purus malesuada congue. 
+          Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? 
+          Etiam posuere lacus quis dolor. Duis viverra diam non justo. Integer rutrum, orci vestibulum ullamcorper ultricies, lacus quam ultricies odio, 
+          vitae placerat pede sem sit amet enim. Fusce suscipit libero eget elit. Integer imperdiet lectus quis justo. Nulla accumsan, elit sit amet varius semper, 
+          nulla mauris mollis quam, tempor suscipit diam nulla vel leo. Etiam dictum tincidunt diam. Maecenas sollicitudin. Etiam dui sem, fermentum vitae, 
+          sagittis id, malesuada in, quam. Mauris dictum facilisis augue. Proin mattis lacinia justo. 
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, 
+          totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p> 
+    </div>
+  );
+}
+
+export default App;
